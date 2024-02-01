7ea6d6763e59e65779c1c622b5b9f0b4819d715e
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-02-01 00:54:22 -0500

    flag commands created

---
+++ src/command.js
@@ -0,0 +1,49 @@
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+
+
+yargs(hideBin(process.argv))
+	.command('batch', ' To Resize, Compress or Convert All Images In The Directory', (yargs) => {
+
+		console.log('batching in progress')
+		return yargs.positional('input', {
+			describe: 'Use resize format of hxw',
+			type: 'string'
+		})
+
+	})
+	.option('resize', {
+		alias: 'r',
+		describe: 'resize the image',
+		type: 'string',
+
+	})
+	.option('quality', {
+		alias: 'q',
+		describe: 'resize the image',
+		type: 'number'
+	})
+	.option('format', {
+		alias: 'f',
+		describe: 'convert the image to a different format',
+		type: 'string'
+	})
+	.check((argv) => {
+		if (argv.resize) {
+			if (argv.resize.trim().toLowerCase().includes('x') !== true) {
+				throw new Error('resize format must be in the format of hxw')
+			}
+			console.log('resizing images...')
+			console.log(argv.resize.trim().includes('x'))
+			let dimensions = argv.resize.toLowerCase().split('x')
+			// function to resize image
+
+		}
+		return true
+	
+	})
+	.demandCommand(1)
+	.parse()
+
+
