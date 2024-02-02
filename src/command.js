3403f4277b9a6d7ae9376b971c0b296b1f46331d
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-02-01 22:14:15 -0500

    image manipulation started and some test images

--- src/command.js
+++ src/command.js
@@ -1,50 +1,42 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-
-
+import { processImages } from './utils.js'
 
 yargs(hideBin(process.argv))
-	.command('batch', ' To Resize, Compress or Convert All Images In The Directory', (yargs) => {
+  .command('batch', ' To Resize, Compress or Convert All Images In The Directory', (yargs) => {
 
-		console.log('batching in progress')
-		return yargs.positional('input', {
-			describe: 'Use resize format of hxw',
-			type: 'string'
-		})
+    return yargs.positional('input', {
+      describe: 'Use resize format of hxw',
+      type: 'string'
+    })
 
-	})
-	.option('resize', {
-		alias: 'r',
-		describe: 'resize the image',
-		type: 'string',
+  })
+  .option('resize', {
+    alias: 'r',
+    describe: 'resize the image',
+    type: 'string',
 
-	})
-	.option('quality', {
-		alias: 'q',
-		describe: 'resize the image',
-		type: 'number'
-	})
-	.option('format', {
-		alias: 'f',
-		describe: 'convert the image to a different format',
-		type: 'string'
-	})
-	.check((argv, options) => {
-		console.log('checking the options', argv)
-		if (argv.r || argv.q || argv.f) {
-			if (argv.resize.trim().toLowerCase().includes('x') !== true) {
-				throw new Error('resize format must be in the format of hxw')
-			}
-			console.log('resizing images...')
-		
-			let dimensions = argv.resize.toLowerCase().split('x')
-			// function to resize image
-
-		}
-		return true
-	
-	})
-	.demandCommand(1)
-	.parse()
+  })
+  .option('quality', {
+    alias: 'q',
+    describe: 'resize the image',
+    type: 'number'
+  })
+  .option('format', {
+    alias: 'f',
+    describe: 'convert the image to a different format',
+    type: 'string'
+  })
+  .check((argv, options) => {
+    if (argv.r || argv.q || argv.f) {
+      if (argv.resize.trim().toLowerCase().includes('x') !== true) {
+        throw new Error('resize format must be in the format of hxw')
+      }
+      processImages(argv)
+    }
+    return true
+  })
+  .demandCommand(1)
+  .parse()
 
 
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,40 @@
-import sharp from 'sharp'
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+
+
+export async function processImages(options) {
+  const directory = process.cwd();
+  let files = fs.readdirSync(directory);
+
+
+  let images = files.filter(file => {
+    return ['.png', '.jpg', '.webp'].includes(path.extname(file).toLowerCase());
+  });
+
+  if (images.length === 0) {
+    console.log("No images found");
+    return;
+  }
+
+  for (let image of images) {
+
+    await processImage(path.join(directory, image), options);
+  }
+}
+
+async function processImage(imagePath, options) {
+  console.log("Processing image:", imagePath);
+
+  const outputFilename = path.join(
+    path.dirname(imagePath),
+    path.basename(imagePath, path.extname(imagePath)) + '.png'
+  );
+
+  if (options.f) {
+    await sharp(imagePath)
+      .toFormat('png')
+      .toFile(outputFilename);
+    console.log("Output file:", outputFilename);
+  }
+}
